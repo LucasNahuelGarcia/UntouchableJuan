fd2147ec3e80db41ed369678dc4467a3a0002f16
lucas <nahuellucasgarcia@gmail.com>
2026-05-09 17:48:55 -0300

    Quitar archivos viejos y rotos, reordenar proyecto

---
+++ Assets/_Project/SFX/FootstepTerrainMapper.cs
@@ -0,0 +1,15 @@
+using System;
+using Audio;
+
+namespace Core
+{
+    internal class FootstepTerrainMapper
+    {
+        public AudioEvent FootStepEvent { get; internal set; }
+
+        internal bool ContainedInName(string terrainName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
